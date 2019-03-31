a4973d8aa3a826bafbf1715c4de0294f3b5b6eac
Parth Patel <parthpp1996@gmail.com>
2019-03-31 17:53:09 -0400

    Update Trees.py

--- Algorithms/Trees.py
+++ Algorithms/Trees.py
@@ -1,21 +1,22 @@
 class TreeNode:
+  # initialize
   def __init__(self, value):
-    self.value = value # data
-    self.children = [] # references to other nodes
-
+    self.value = value 
+    self.children = [] 
+    
+  # add child-parent node 
   def add_child(self, child_node):
-    # creates parent-child relationship
     print("Adding " + child_node.value)
     self.children.append(child_node) 
     
+  # removes parent-child relationship
   def remove_child(self, child_node):
-    # removes parent-child relationship
     print("Removing " + child_node.value + " from " + self.value)
     self.children = [child for child in self.children 
                      if child is not child_node]
 
+    
   def traverse(self):
-    # moves through each node referenced from self downwards
     nodes_to_visit = [self]
     while len(nodes_to_visit) > 0:
       current_node = nodes_to_visit.pop()
